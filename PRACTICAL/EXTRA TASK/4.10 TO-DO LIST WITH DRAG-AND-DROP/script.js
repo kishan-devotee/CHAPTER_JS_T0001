c1f41a95054c5394a8d51ccdca66f5cb5c6896dc
DESKTOP-6GMA9VH\ADMIN <kishan.devotee@gmail.com>
2024-07-12 15:57:24 +0530

    WORKING ON TODO DRAG & DROP.

---
+++ PRACTICAL/EXTRA TASK/4.10 TO-DO LIST WITH DRAG-AND-DROP/script.js
@@ -0,0 +1,21 @@
+var btn = document.querySelector('.add');
+var remove = document.querySelector('.draggable');
+
+
+function addNewItem() {
+  var newItem = document.querySelector('.input').value;
+  if (newItem != '') {
+    document.querySelector('.input').value = '';
+    var li = document.createElement('li');
+    var attr = document.createAttribute('draggable');
+    var ul = document.querySelector('ul');
+    li.className = 'draggable';
+    attr.value = 'true';
+    li.setAttributeNode(attr);
+    li.appendChild(document.createTextNode(newItem));
+    ul.appendChild(li);
+    addEventsDragAndDrop(li);
+  }
+}
+
+btn.addEventListener('click', addNewItem);
